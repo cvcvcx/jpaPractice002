19308e68133e5af88ff1dd556df25c5fa5ad28d0
cvcvcx9 <cvcvcx9@gmaill.com>
2023-03-23 17:12:05 +0900

    두번째 연습장

---
+++ src/main/java/com/example/jpaPrectice002/JpaPrectice002Application.java
@@ -0,0 +1,13 @@
+package com.example.jpaPrectice002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPrectice002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPrectice002Application.class, args);
+	}
+
+}
